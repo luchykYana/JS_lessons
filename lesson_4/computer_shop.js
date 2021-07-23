def2383d7ee3efd6a8c6caeb55ba7e6ce49bea86
luchykYana <yanaluchyk@gmail.com>
2021-07-23 16:43:39 +0300

    JS_lesson_4 computer_shop and lesson6

--- lesson_4/computer_shop.js
+++ lesson_4/computer_shop.js
@@ -33,47 +33,136 @@
 // що на цьому відрі ігри не запускаються.
 
 
-class Computer{
-
-    constructor(ram, cpu_power, model) {
-        this.ram = ram;
-        if(cpu_power > 0 && cpu_power < 1000){
-            this.cpu_power = cpu_power;
-        }
-        this.model = model;
-    }
-    turn_on(){
-        console.log('Включився');
-    }
-}
-
-class Laptop extends Computer{
-
-    constructor(ram, cpu_power, model, diagonal) {
-        super(ram, cpu_power, model);
-        this.diagonal = diagonal;
-        this.battery = cpu_power / (diagonal * ram);
-    }
-    turn_on() {
-        super.turn_on();
-        console.log('ноутбук');
-    }
-}
-
-class Ultrabook extends Laptop{
-
-    constructor(ram, cpu_power, model, diagonal, weight) {
-        super(ram, cpu_power, model, diagonal);
-        this.battery = cpu_power / (diagonal * ram);
-        this.weight = weight;
-    }
-    turn_on() {
-        if(this.weight > 2 && this.battery < 4){
-            console.log('Error');
-        }
-        else{
-            super.turn_on();
-            console('ультрабук');
-        }
-    }
-}
+// class Computer{
+//
+//     constructor(ram, cpu_power, model) {
+//         this.ram = ram;
+//         if(cpu_power > 0 && cpu_power < 1000){
+//             this.cpu_power = cpu_power;
+//         }
+//         this.model = model;
+//     }
+//     turn_on(){
+//         console.log('Включився');
+//     }
+// }
+//
+// class Laptop extends Computer{
+//
+//     constructor(ram, cpu_power, model, diagonal) {
+//         super(ram, cpu_power, model);
+//         this.diagonal = diagonal;
+//         this.battery = cpu_power / (diagonal * ram);
+//     }
+//     turn_on() {
+//         super.turn_on();
+//         console.log('ноутбук');
+//     }
+// }
+//
+// class Ultrabook extends Laptop{
+//
+//     constructor(ram, cpu_power, model, diagonal, weight) {
+//         super(ram, cpu_power, model, diagonal);
+//         this.battery = cpu_power / (diagonal * ram);
+//         this.weight = weight;
+//     }
+//     turn_on() {
+//         if(this.weight > 2 && this.battery < 4){
+//             console.log('Error');
+//         }
+//         else{
+//             super.turn_on();
+//             console.log('моделі \'ультрабук\'');
+//         }
+//     }
+// }
+//
+// class PC extends Computer{
+//
+//     constructor(ram, cpu_power, model, fpc = 0) {
+//         super(ram, cpu_power, model);
+//         this.fpc = fpc = cpu_power / ram;
+//     }
+//
+//     turn_on() {
+//         super.turn_on();
+//         console.log('ПК');
+//     }
+//     games(game_name){
+//         console.log('You are playing ' + game_name + 'with ' + this.fpc + ' FPC');
+//     }
+//     upgrade_cpu_power(percent){
+//         if(percent < 10 && percent > 0){
+//             this.cpu_power += this.cpu_power / 100 * percent;
+//         }
+//     }
+//     upgrade_ram(){
+//         this.ram *= 2;
+//     }
+//     change_ram(ram){
+//         this.ram = ram;
+//     }
+//     change_cpu_power(cpu){
+//         this.cpu_power = cpu;
+//     }
+//     change_model(model){
+//         this.model = model;
+//     }
+// }
+//
+// class Gaming_Computer extends PC{
+//
+//     constructor(ram, cpu_power, model, fpc = 0) {
+//         super(ram, cpu_power, model, fpc);
+//         this.fpc_game = this.fpc * 2;
+//     }
+//
+//     turn_on() {
+//         super.turn_on();
+//         console.log('для ігор');
+//     }
+//     games(game_name) {
+//         let temp = this.cpu_power;
+//         this.cpu_power -= this.cpu_power/100*0.1;
+//         if(this.cpu_power < 500 && this.ram < 8){
+//             console.log('На цьому відрі ігри не запускаються');
+//             this.cpu_power = temp;
+//             return;
+//         }
+//         else{
+//             super.games(game_name);
+//             this.cpu_power = temp;
+//         }
+//         return;
+//     }
+// }
+//
+//
+// let computer = new Computer(8, 500, '23e45df');
+// let laptop = new Laptop(16, 800, 'gs68263', 22);
+// let ultrabook = new Ultrabook(16, 900, '234guiy',24, 1.6);
+// let pc = new PC(8, 600,'435giy');
+// let game_computer = new Gaming_Computer('16', 900, '35ftydkty');
+//
+// console.log(computer);
+// computer.turn_on();
+// console.log('---------------------------');
+//
+// console.log(laptop);
+// laptop.turn_on();
+// console.log('---------------------------');
+//
+// console.log(ultrabook);
+// ultrabook.turn_on();
+// console.log('---------------------------');
+//
+// console.log(pc);
+// pc.turn_on();
+// pc.games('Long Dark 3 ');
+// console.log('---------------------------');
+//
+// console.log(game_computer);
+// game_computer.turn_on();
+// game_computer.games('Long Dark 3 ');
+// console.log(game_computer);
--- lesson_4/kminV_lesson6.js
+++ lesson_4/kminV_lesson6.js
@@ -1,24 +1,91 @@
-// 1) Створити масив з 20 чисел та:
+// // 1) Створити масив з 20 чисел та:
+// let mas = [61, 56, 57, 36, 41, 5, 27, 85, 51, 4, 48, 21, 64, 6, 16, 3, 55, 47, 2, 34];
+//
+// //     a) відсортувати його від меншого до більшого.
+// mas.sort((a,b) => a - b);
+// console.log(mas);
+//
+// //     b) відсортувати його від більшого до меншого.
+// mas.sort((a,b) => b - a);
+// console.log(mas);
+//
+// //     c) Відфілтрувати числа які є кратними 3.
+// let filter = mas.filter(a => a % 3 === 0);
+// console.log(filter);
+//
+// // d) Відфілтрувати числа які є більшими за 10.
+// let filter2 = mas.filter(a => a > 10);
+// console.log(filter2);
+//
+// // e) Проітерувати його forEach та вивести кожеш елмент за допомогою document.write
+// mas.forEach((a) => document.write(a + ' '));
+//
+// // f) За допомогою map збільшити кожен елемент в масиві в три рази.
+// let map = mas.map((a) => a*3);
+// console.log(map);
+//
+// //     g) Порахувати загальну суму всіх елментів у масиві (reduce)
+//
+// let sum = mas.reduce((a,b) => a + b);
+// console.log(sum);
 
-// f) За допомогою map збільшити кожен елемент в масиві в три рази.
-//     g) Порахувати загальну суму всіх елментів у масиві (reduce)
 
-let mas = [61, 56, 57, 36, 41, 5, 27, 85, 51, 4, 48, 21, 64, 6, 16, 3, 55, 47, 2, 34];
 
-//     a) відсортувати його від меншого до більшого.
-mas.sort((a,b) => a - b);
-console.log(mas);
+// // 2) Створити масив з 20 стрічок та:
+// let str = ['htg', 'thegfds', 'aergfvs', 'rgssss4w', 'gsefdv', 'heagfd', 'qwa', 'nhgfb', 'q', 'hngbfvds',
+//            'mnbg', 'rgsvd', 'rbs', 'qwerty', 'gbfvdc', 'po', 'lkjhgfdsa', 'kjhgf', 'nbvc', 'rfctfgv'];
+//
+// // // a) Відсортувати його в алфавітному порядку
+// // console.log(str.sort());
+//
+// // // b) Відсортувати в зворотньому порядку
+// // console.log(str.reverse());
+//
+// // // c) Отримати в новому масиві тільки ті слова, довжина яких більша за 4 букви (filter)
+// // let filter = str.filter(a => a.length > 4);
+// // console.log(filter);
+//
+// // // d) Змінити кожне слово в масиві додавши на початок 'Sam says *ваше слово*'
+// // let map = str.map(a => 'Sam says ' + a);
+// // console.log(map);
 
-//     b) відсортувати його від більшого до меншого.
-mas.sort((a,b) => b - a);
-console.log(mas);
 
-//     c) Відфілтрувати числа які є кратними 3.
-let filter = mas.filter(a => a % 3 === 0);
-console.log(filter);
 
-// d) Відфілтрувати числа які є більшими за 10.
-let filter2 = mas.filter(a => a > 10);
-console.log(filter2);
+// // 3) Все робити тільки за допомогою методів масивів!
+// //     Дано масив :
+//     const users = [
+//         {name: 'vasya', age: 31, isMarried: false},
+//         {name: 'petya', age: 30, isMarried: true},
+//         {name: 'kolya', age: 29, isMarried: true},
+//         {name: 'olya', age: 28, isMarried: false},
+//         {name: 'max', age: 30, isMarried: true},
+//         {name: 'anya', age: 31, isMarried: false},
+//         {name: 'oleg', age: 28, isMarried: false},
+//         {name: 'andrey', age: 29, isMarried: true},
+//         {name: 'masha', age: 30, isMarried: true},
+//         {name: 'olya', age: 31, isMarried: false},
+//         {name: 'max', age: 31, isMarried: true}
+//     ];
+// // // a) відсортувати його за  віком (зростання , а потім окремо спадання)
+// console.log(users.sort((a, b) => a.age - b.age));
+// console.log(users.sort((a, b) => b.age - a.age));
+//
+// // // b) відсортувати його за кількістю знаків в імені  (зростання , а потім окремо спадання)
+// console.log(users.sort((a, b) => a.name.length - b.name.length ));
+// console.log(users.sort((a, b) => b.name.length  - a.name.length ));
+//
+// // c) пройтись по ньому та додати кожному юзеру поле id - яке характеризує унікальний індентифікатор
+// // (По якому принципу його створювати - ваше рішення),
+// // та зберегти це в новий масив (первинний масив залишиться без змін)
+// let map = users.map(function (user, index){
+//       user.id = index+1;
+//       return user;
+// });
+// console.log(map);
+//
+// // // d) відсортувати його за індентифікатором
+// map.sort((a,b) => b.id - a.id);
+// console.log(map);
+//
+// // e) Всі хто одружений мають попасти у новий масив та отрмати квартиру (reduce)
 
-// e) Проітерувати його forEach та вивести кожеш елмент за допомогою document.write

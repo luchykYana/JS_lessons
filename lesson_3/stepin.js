4a5f48793ad27366a12fb6821dd3518d46ff941f
luchykYana <yanaluchyk@gmail.com>
2021-07-24 18:20:52 +0300

    step2

--- lesson_3/stepin.js
+++ lesson_3/stepin.js
@@ -3,15 +3,31 @@
 //Якщо ні, "Not step 2"
 
 
-function step2(n){
-    for(let i = 0; i < 100; i++){
-        if(Math.pow(2, i) === n){
-            return i;
-        }
-    }
-    return 'Not step 2';
-}
+// function step2(n){
+//     for(let i = 0; i < 100; i++){
+//         if(Math.pow(2, i) === n){
+//             return i;
+//         }
+//     }
+//     return 'Not step 2';
+// }
+//
+// console.log(step2(1024));
+// console.log(step2(256));
+// console.log(step2(333));
 
-console.log(step2(1024));
-console.log(step2(256));
-console.log(step2(333));
+
+
+// function step2(n){
+//     let i = 0;
+//     while(Math.pow(2, i) <= n){
+//         if(Math.pow(2, i) === n){
+//             return i;
+//         }
+//          i++;
+//     }
+//     return 'Not step 2';
+// }
+// console.log(step2(1099511627776));
+// console.log(step2(256));
+// console.log(step2(333));

b77fd802a3a5d9c5199609f93dcf8514356861ba
luchykYana <yanaluchyk@gmail.com>
2021-08-03 13:36:58 +0300

    1400 and 1200

--- mini_project/index.js
+++ mini_project/index.js
@@ -1,72 +1,170 @@
 
+if(window.matchMedia("(min-width: 1400px)").matches){
+    let h = document.createElement('h1');
+    h.innerHTML = 'XXL';
+    document.body.appendChild(h);
 
-let mainDiv = document.createElement('div');
+    let mainDiv = document.createElement('div');
 
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json())
+        .then(users => {
+            for (const user of users) {
 
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(value => value.json())
-    .then(users => {
-        for (const user of users) {
+                let userDiv = document.createElement('div');
+                let userId = document.createElement('p');
+                let userName = document.createElement('p');
+                let userInfo = document.createElement('div');
+                let btnUserDetails = document.createElement('button');
 
-            let userDiv = document.createElement('div');
-            let userId = document.createElement('p');
-            let userName = document.createElement('p');
-            let userInfo = document.createElement('div');
-            let btnUserDetails = document.createElement('button');
+                userId.innerHTML = `${user.id}.`;
+                userName.innerHTML = `${user.name}`;
+                btnUserDetails.innerHTML = 'details';
 
-            userId.innerHTML = `${user.id}.`;
-            userName.innerHTML = `${user.name}`;
-            btnUserDetails.innerHTML = 'details';
+                btnUserDetails.onmouseover = () => {
+                    btnUserDetails.style.border = '1px solid #59253A';
+                    btnUserDetails.style.backgroundColor = 'white';
+                    btnUserDetails.style.color = '#59253A';
+                    btnUserDetails.style.transition = 'background-color 500ms, color 500ms';
+                    btnUserDetails.style.cursor = 'pointer';
+                }
+                btnUserDetails.onmouseleave = () => {
+                    btnUserDetails.style.backgroundColor = '#78244C';
+                    btnUserDetails.style.border = '1px solid white';
+                    btnUserDetails.style.color = 'white';
+                    btnUserDetails.style.transition = 'background-color 500ms, color 500ms';
+                    btnUserDetails.style.cursor = 'pointer';
+                }
+                btnUserDetails.onclick = () => {
+                    location.href = `user-details.html?user=${JSON.stringify(user)}`;
+                }
 
-            btnUserDetails.onmouseover = () => {
-                btnUserDetails.style.border = '1px solid #59253A';
-                btnUserDetails.style.backgroundColor = 'white';
-                btnUserDetails.style.color = '#59253A';
-                btnUserDetails.style.transition = 'background-color 500ms, color 500ms';
-                btnUserDetails.style.cursor = 'pointer';
-            }
-            btnUserDetails.onmouseleave = () => {
+                userInfo.append(userId, userName);
+                userDiv.append(userInfo, btnUserDetails);
+                mainDiv.appendChild(userDiv);
+
+                userDiv.style.width = '600px';
+                userDiv.style.height = '170px';
+                userDiv.style.marginTop = '40px';
+                userDiv.style.padding = '20px';
+                userDiv.style.backgroundColor = '#59253A';
+                userDiv.style.color = 'white';
+                userDiv.style.fontSize = '40px';
+                userInfo.style.display = 'flex';
+                userInfo.style.justifyContent = 'center';
+                btnUserDetails.style.width = '50%';
+                btnUserDetails.style.height = '50px';
+                btnUserDetails.style.marginTop = '20px';
+                btnUserDetails.style.display = 'block';
+                btnUserDetails.style.marginLeft = 'auto';
+                btnUserDetails.style.marginRight = 'auto';
+                btnUserDetails.style.fontSize = '25px';
                 btnUserDetails.style.backgroundColor = '#78244C';
                 btnUserDetails.style.border = '1px solid white';
+                btnUserDetails.style.borderRadius = '10px';
                 btnUserDetails.style.color = 'white';
-                btnUserDetails.style.transition = 'background-color 500ms, color 500ms';
-                btnUserDetails.style.cursor = 'pointer';
+                userId.style.marginRight = '10px';
             }
-            btnUserDetails.onclick = () => {
-                location.href = `user-details.html?user=${JSON.stringify(user)}`;
+        })
+
+    document.body.appendChild(mainDiv);
+
+    mainDiv.style.display = 'flex';
+    mainDiv.style.flexWrap = 'wrap';
+    mainDiv.style.justifyContent = 'space-evenly';
+    document.body.style.paddingBottom = '40px';
+
+}else if(window.matchMedia("(min-width: 1200px)").matches){
+    let h = document.createElement('h1');
+    h.innerHTML = 'XL';
+    document.body.appendChild(h);
+
+    let mainDiv = document.createElement('div');
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json())
+        .then(users => {
+            for (const user of users) {
+
+                let userDiv = document.createElement('div');
+                let userId = document.createElement('p');
+                let userName = document.createElement('p');
+                let userInfo = document.createElement('div');
+                let btnUserDetails = document.createElement('button');
+
+                userId.innerHTML = `${user.id}.`;
+                userName.innerHTML = `${user.name}`;
+                btnUserDetails.innerHTML = 'details';
+
+                btnUserDetails.onmouseover = () => {
+                    btnUserDetails.style.border = '1px solid #59253A';
+                    btnUserDetails.style.backgroundColor = 'white';
+                    btnUserDetails.style.color = '#59253A';
+                    btnUserDetails.style.transition = 'background-color 500ms, color 500ms';
+                    btnUserDetails.style.cursor = 'pointer';
+                }
+                btnUserDetails.onmouseleave = () => {
+                    btnUserDetails.style.backgroundColor = '#78244C';
+                    btnUserDetails.style.border = '1px solid white';
+                    btnUserDetails.style.color = 'white';
+                    btnUserDetails.style.transition = 'background-color 500ms, color 500ms';
+                    btnUserDetails.style.cursor = 'pointer';
+                }
+                btnUserDetails.onclick = () => {
+                    location.href = `user-details.html?user=${JSON.stringify(user)}`;
+                }
+
+                userInfo.append(userId, userName);
+                userDiv.append(userInfo, btnUserDetails);
+                mainDiv.appendChild(userDiv);
+
+                userDiv.style.width = '500px';
+                userDiv.style.height = '170px';
+                userDiv.style.marginTop = '40px';
+                userDiv.style.padding = '20px';
+                userDiv.style.backgroundColor = '#59253A';
+                userDiv.style.color = 'white';
+                userDiv.style.fontSize = '35px';
+                userInfo.style.display = 'flex';
+                userInfo.style.justifyContent = 'center';
+                btnUserDetails.style.width = '50%';
+                btnUserDetails.style.height = '40px';
+                btnUserDetails.style.marginTop = '30px';
+                btnUserDetails.style.display = 'block';
+                btnUserDetails.style.marginLeft = 'auto';
+                btnUserDetails.style.marginRight = 'auto';
+                btnUserDetails.style.fontSize = '20px';
+                btnUserDetails.style.backgroundColor = '#78244C';
+                btnUserDetails.style.border = '1px solid white';
+                btnUserDetails.style.borderRadius = '10px';
+                btnUserDetails.style.color = 'white';
+                userId.style.marginRight = '10px';
             }
+        })
 
-            userInfo.append(userId, userName);
-            userDiv.append(userInfo, btnUserDetails);
-            mainDiv.appendChild(userDiv);
+    document.body.appendChild(mainDiv);
 
-            userDiv.style.width = '600px';
-            userDiv.style.height = '170px';
-            userDiv.style.marginTop = '40px';
-            userDiv.style.padding = '20px';
-            userDiv.style.backgroundColor = '#59253A';
-            userDiv.style.color = 'white';
-            userDiv.style.fontSize = '40px';
-            userInfo.style.display = 'flex';
-            userInfo.style.justifyContent = 'center';
-            btnUserDetails.style.width = '50%';
-            btnUserDetails.style.height = '50px';
-            btnUserDetails.style.marginTop = '20px';
-            btnUserDetails.style.display = 'block';
-            btnUserDetails.style.marginLeft = 'auto';
-            btnUserDetails.style.marginRight = 'auto';
-            btnUserDetails.style.fontSize = '25px';
-            btnUserDetails.style.backgroundColor = '#78244C';
-            btnUserDetails.style.border = '1px solid white';
-            btnUserDetails.style.borderRadius = '10px';
-            btnUserDetails.style.color = 'white';
-            userId.style.marginRight = '10px';
-        }
-    })
+    mainDiv.style.display = 'flex';
+    mainDiv.style.flexWrap = 'wrap';
+    mainDiv.style.justifyContent = 'space-between';
+    mainDiv.style.padding = '20px 60px';
+    document.body.style.paddingBottom = '40px';
 
-document.body.appendChild(mainDiv);
 
-mainDiv.style.display = 'flex';
-mainDiv.style.flexWrap = 'wrap';
-mainDiv.style.justifyContent = 'space-evenly';
-document.body.style.paddingBottom = '40px';
+}else if(window.matchMedia("(min-width: 992px)").matches){
+    let h = document.createElement('h1');
+    h.innerHTML = 'LG';
+    document.body.appendChild(h);
+}else if(window.matchMedia("(min-width: 768px)").matches){
+    let h = document.createElement('h1');
+    h.innerHTML = 'MD';
+    document.body.appendChild(h);
+}else if(window.matchMedia("(min-width: 576px)").matches){
+    let h = document.createElement('h1');
+    h.innerHTML = 'SM';
+    document.body.appendChild(h);
+}else {
+    let h = document.createElement('h1');
+    h.innerHTML = 'XS';
+    document.body.appendChild(h);
+}
--- mini_project/post-details.js
+++ mini_project/post-details.js
@@ -4,51 +4,126 @@ const mail = JSON.parse(url.searchParams.get('post'));
 
 let postDiv = document.createElement('div');
 
-for (const key in mail) {
-    let keyDiv = document.createElement('p');
-    keyDiv.innerHTML = `<span class="red">${key}:</span> ${mail[key]}`;
-    postDiv.appendChild(keyDiv);
-}
 
-let commentsDiv = document.createElement('div');
+if(window.matchMedia("(min-width: 1400px)").matches){
+    let h = document.createElement('h1');
+    h.innerHTML = 'XXL';
+    document.body.appendChild(h);
 
-fetch(`https://jsonplaceholder.typicode.com/posts/${mail.id}/comments`)
-    .then(value => value.json())
-    .then(comments => {
-        for (const comment of comments) {
-            let commentDiv = document.createElement('div');
-            for (const key in comment) {
-                let rowDiv = document.createElement('p');
-                rowDiv.innerHTML = `<span class="blue">${key}:</span> ${comment[key]}`;
-                commentDiv.appendChild(rowDiv);
-                rowDiv.style.marginBottom = '5px';
+    for (const key in mail) {
+        let keyDiv = document.createElement('p');
+        keyDiv.innerHTML = `<span class="red">${key}:</span> ${mail[key]}`;
+        postDiv.appendChild(keyDiv);
+    }
+
+    let commentsDiv = document.createElement('div');
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/${mail.id}/comments`)
+        .then(value => value.json())
+        .then(comments => {
+            for (const comment of comments) {
+                let commentDiv = document.createElement('div');
+                for (const key in comment) {
+                    let rowDiv = document.createElement('p');
+                    rowDiv.innerHTML = `<span class="blue">${key}:</span> ${comment[key]}`;
+                    commentDiv.appendChild(rowDiv);
+                    rowDiv.style.marginBottom = '5px';
+                }
+                commentsDiv.appendChild(commentDiv);
+                commentDiv.style.width = '330px';
+                commentDiv.style.height = '350px';
+                commentDiv.style.backgroundColor = '#2D4159';
+                commentDiv.style.color = 'white';
+                commentDiv.style.padding = '20px';
+                commentDiv.style.fontSize = '20px';
+                commentDiv.style.marginBottom = '33px';
             }
-            commentsDiv.appendChild(commentDiv);
-            commentDiv.style.width = '330px';
-            commentDiv.style.height = '350px';
-            commentDiv.style.backgroundColor = '#2D4159';
-            commentDiv.style.color = 'white';
-            commentDiv.style.padding = '20px';
-            commentDiv.style.fontSize = '20px';
-            commentDiv.style.marginBottom = '33px';
-        }
-    })
+        })
 
-document.body.append(postDiv, commentsDiv);
+    document.body.append(postDiv, commentsDiv);
 
 
 
-postDiv.style.backgroundColor = '#59253A';
-postDiv.style.color = 'white';
-postDiv.style.fontSize = '30px';
-postDiv.style.margin = '50px';
-postDiv.style.padding = '40px';
+    postDiv.style.backgroundColor = '#59253A';
+    postDiv.style.color = 'white';
+    postDiv.style.fontSize = '30px';
+    postDiv.style.margin = '50px';
+    postDiv.style.padding = '40px';
 
-commentsDiv.style.margin = '50px';
-commentsDiv.style.display = 'flex';
-commentsDiv.style.flexWrap = 'wrap';
-commentsDiv.style.gridColumnGap = '33px';
+    commentsDiv.style.margin = '50px';
+    commentsDiv.style.display = 'flex';
+    commentsDiv.style.flexWrap = 'wrap';
+    commentsDiv.style.justifyContent = 'space-between';
 
-document.body.style.paddingBottom = '40px';
+    document.body.style.paddingBottom = '40px';
 
 
+}else if(window.matchMedia("(min-width: 1200px)").matches){
+    let h = document.createElement('h1');
+    h.innerHTML = 'XL';
+    document.body.appendChild(h);
+
+    for (const key in mail) {
+        let keyDiv = document.createElement('p');
+        keyDiv.innerHTML = `<span class="red">${key}:</span> ${mail[key]}`;
+        postDiv.appendChild(keyDiv);
+    }
+
+    let commentsDiv = document.createElement('div');
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/${mail.id}/comments`)
+        .then(value => value.json())
+        .then(comments => {
+            for (const comment of comments) {
+                let commentDiv = document.createElement('div');
+                for (const key in comment) {
+                    let rowDiv = document.createElement('p');
+                    rowDiv.innerHTML = `<span class="blue">${key}:</span> ${comment[key]}`;
+                    commentDiv.appendChild(rowDiv);
+                    rowDiv.style.marginBottom = '5px';
+                }
+                commentsDiv.appendChild(commentDiv);
+                commentDiv.style.width = '250px';
+                commentDiv.style.height = '330px';
+                commentDiv.style.backgroundColor = '#2D4159';
+                commentDiv.style.color = 'white';
+                commentDiv.style.padding = '20px';
+                commentDiv.style.fontSize = '16px';
+                commentDiv.style.marginBottom = '33px';
+            }
+        })
+
+    document.body.append(postDiv, commentsDiv);
+
+
+
+    postDiv.style.backgroundColor = '#59253A';
+    postDiv.style.color = 'white';
+    postDiv.style.fontSize = '25px';
+    postDiv.style.margin = '50px';
+    postDiv.style.padding = '40px';
+
+    commentsDiv.style.margin = '50px';
+    commentsDiv.style.display = 'flex';
+    commentsDiv.style.flexWrap = 'wrap';
+    commentsDiv.style.justifyContent = 'space-between';
+
+    document.body.style.paddingBottom = '40px';
+
+}else if(window.matchMedia("(min-width: 992px)").matches){
+    let h = document.createElement('h1');
+    h.innerHTML = 'LG';
+    document.body.appendChild(h);
+}else if(window.matchMedia("(min-width: 768px)").matches){
+    let h = document.createElement('h1');
+    h.innerHTML = 'MD';
+    document.body.appendChild(h);
+}else if(window.matchMedia("(min-width: 576px)").matches){
+    let h = document.createElement('h1');
+    h.innerHTML = 'SM';
+    document.body.appendChild(h);
+}else {
+    let h = document.createElement('h1');
+    h.innerHTML = 'XS';
+    document.body.appendChild(h);
+}
